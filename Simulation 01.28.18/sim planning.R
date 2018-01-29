3d462b12a03d54ad9d6f7fd546a4b924aa94282e
Multach <matt.multach87@gmail.com>
2018-01-29 09:09:28 -0800

    Sim Planning Update 01.29.18 B

--- Simulation 01.28.18/sim planning.R
+++ Simulation 01.28.18/sim planning.R
@@ -10,18 +10,29 @@ ghdist
 #One-sample version????
 #create 1 normal 100k population
 
-#Create 2 normal 100k populations --> draw 1 sample of size n from each --> check normality
-##--> group comparisons --> repeat * 10000
+#tests: (4)
+##two-sample t test
+##Cliff's WMW (192)
+##20% trim: Yuen's Method (166)
+##Median: pb method 5.4.2 (185)?
 
-#Vary: Number tests? 
+####Vary: Number tests? 
+#####Add: 10% trim?
+#####Add: bootstrap-t tests?
+#####Add: Welch's?
+#####Add: compare variances/measures of scale?
+
+
+#Create 2 normal 100k populations --> draw 1 sample of size n from each --> check normality
+##--> group comparisons --> repeat * 10000 --> look at distribution and quantiles of p-values
+
 
 #nrows = 10,000
 ##for each repetition
-#ncols = # of tests
+#ncols = # of tests (4)
 ##cell [i,j] corresponds to a binary 0/1 for whether test j for iteration i incorrectly rejected
 ###null hypothesis of no difference
 
 #n's = 25 , 50 , 100 , 250 , 500 , 1000 , 2000 , 5000 , 10000
 ##9 simulations, 9 datasets
 
-#tests: two-sample t test, Cliff's WMW, 10% Trim, 20% trim, Median
